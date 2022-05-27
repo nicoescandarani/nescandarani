866f3ad2301b1f09c6df315f30209a0a5687b5fa
Nicolas Escandarani <nico@escandarani.com.ar>
2022-05-27 01:14:18 -0300

    [UPDATE] master: Updated assets paths.

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -14,81 +14,81 @@ export class InfoService {
       title: 'Guess The Number Game',
       shortDescription: 'Simple guessing game',
       description: 'A simple game where the user has to guess a number between 1 and 100.',
-      image: '../../assets/img/GuessTheNumberGame.png',
+      image: 'assets/img/GuessTheNumberGame.png',
     },
     {
       url: 'https://nicoescandarani.github.io/this-is-a-mug/',
       title: 'This is a mug',
       shortDescription: 'Three.js mug project',
       description: 'Just an interactive 3D mug powered by three.js.',
-      image: '../../assets/img/this-is-a-mug.png',
+      image: 'assets/img/this-is-a-mug.png',
     },
     {
       url: 'https://nescanda.com/kingdomFight/',
       title: 'Fight For Rywhndyr',
       shortDescription: 'RPG like game',
       description: 'Simple and fun RPG game.',
-      image: '../../assets/img/fightForRywndhyr.png',
+      image: 'assets/img/fightForRywndhyr.png',
     },
     {
       url: 'https://nescanda.com/piano/',
       title: 'Piano',
       shortDescription: 'Interactive piano',
       description: 'One octave piano fully made with JavaScript.',
-      image: '../../assets/img/Piano.png',
+      image: 'assets/img/Piano.png',
     },
     {
       url: 'https://nescanda.com/tictactoe/',
       title: 'Space Tic - Tac - Toe',
       shortDescription: 'Parallax Tic - Tac - Toe game',
       description: 'Simple and fun tic - tac - toe game with mouse parallax.',
-      image: '../../assets/img/Tictactoe.jpeg',
+      image: 'assets/img/Tictactoe.jpeg',
     }
     // {
     //   url: 'https://www.nescanda.com/space/',
     //   title: 'Let´s Go To Space',
     //   shortDescription: 'Parallax characters',
     //   description: 'Simple and fun interactive website with mouse parallax and animations. Try clicking the astronaut in the middle.',
-    //   image: '../../assets/img/Space.png',
+    //   image: 'assets/img/Space.png',
     // }
   ];
 
   techs: Tech[] = [
     {
       name: 'Angular',
-      img: '../assets/img/angular.svg'
+      img: 'assets/img/angular.svg'
     },
     {
       name: 'Vuejs',
-      img: '../assets/img/vuejs.svg'
+      img: 'assets/img/vuejs.svg'
     },
     {
       name: 'Nodejs',
-      img: '../assets/img/nodejs.svg'
+      img: 'assets/img/nodejs.svg'
     },
     {
       name: 'Java',
-      img: '../assets/img/java.svg'
+      img: 'assets/img/java.svg'
     },
     {
       name: 'Threejs',
-      img: '../assets/img/threejs.svg'
+      img: 'assets/img/threejs.svg'
     },
     {
       name: 'Firebase',
-      img: '../assets/img/firebase.svg'
+      img: 'assets/img/firebase.svg'
     },
     {
       name: 'Sass | Scss',
-      img: '../assets/img/scss.svg'
+      img: 'assets/img/scss.svg'
     },
     {
       name: 'Mongodb',
-      img: '../assets/img/mongodb.svg'
+      img: 'assets/img/mongodb.svg'
     },
     {
       name: 'Figma',
-      img: '../assets/img/figma.svg'
+      img: 'assets/img/figma.svg'
     }
   ];
 
